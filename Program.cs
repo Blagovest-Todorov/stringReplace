f1747c63cce2f35a0072cf372b77aa0f2bd139f8
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-20 11:59:08 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.substrings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = "Peshoe";
+            // text.Replace('e', '!'); //take the string text, everywhere You see symbol-e, put symbol !;
+            // Console.WriteLine(text);//dont work, it creates new string! -> new variable
+            string newString = text.Replace('e', '!');
+            Console.WriteLine(newString);
+
+        }
+    }
+}
